f46aeca5c23745225d59d3f06b6bd4549e8fcee3
Luiz Pelizaro <luiz.pelizaro127@hotmail.com>
2023-12-03 15:14:05 -0300

    update stack

--- src/exercises/stack_implementetion/stack_thread_safe.cpp
+++ src/exercises/stack_implementetion/stack_thread_safe.cpp
@@ -1,3 +1,54 @@
 //
 // Created by siera on 02/12/2023.
 //
+#include "stack_thread_safe.h"
+
+#include <iostream>
+#include <thread>
+#include <vector>
+
+int main() {
+    // Criando uma instância da pilha thread-safe para inteiros
+    stack_thread_safe<int> myStack;
+
+    // Função para realizar operações de push e pop na pilha
+    auto stackOperations = [&myStack](const int id) {
+        for (int i = 0; i < 5; ++i) {
+            myStack.push(id * 10 + i);
+            std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+            try {
+                int value;
+                myStack.pop(value);
+                std::cout << "Thread " << id << " popped: " << value << std::endl;
+            } catch (const std::runtime_error& e) {
+                std::cout << "Thread " << id << " exception: " << e.what() << std::endl;
+            }
+        }
+    };
+
+    // Criando threads para realizar operações na pilha
+    std::vector<std::thread> threads;
+    threads.reserve(3);
+    for (int i = 0; i < 3; ++i) {
+        threads.emplace_back(stackOperations, i); // emplace_back é utilizada para adicionar um novo elemento no final de um contêine
+    }
+
+    // Aguardando todas as threads terminarem
+    for (auto& thread : threads) {
+        thread.join();
+    }
+
+    // Imprimindo o estado final da pilha
+    std::cout << "Final Stack Size: " << myStack.size() << std::endl;
+    while (!myStack.empty()) {
+        try {
+            const int value = myStack.top();
+            std::cout << "Final Stack Top: " << value << std::endl;
+            myStack.pop();
+        } catch (const std::runtime_error& e) {
+            std::cout << "Final Stack Exception: " << e.what() << std::endl;
+        }
+    }
+
+    return 0;
+}
--- src/exercises/stack_implementetion/stack_thread_safe.h
+++ src/exercises/stack_implementetion/stack_thread_safe.h
@@ -5,38 +5,89 @@
 #ifndef STACK_THREAD_SAFE_H
 #define STACK_THREAD_SAFE_H
 #include <iostream>
+#include <memory>
 #include <mutex>
 #include <stack>
-#include <thread>
 
+/**
+ * @brief Uma pilha segura para threads que suporta operações push, pop, top, empty e size.
+ * @tparam T Tipo dos elementos armazenados na pilha.
+ */
 template<typename T>
 class stack_thread_safe {
-    std::stack<T> stk;
-    std::mutex m;
+    std::stack<std::shared_ptr<T>> stk; /**< Pilha que armazena ponteiros compartilhados para elementos do tipo T. */
+    std::mutex m; /**< Mutex para garantir a sincronização ao acessar a pilha. */
 
 public:
+    /**
+     * @brief Adiciona um elemento à pilha.
+     * @param element Elemento a ser adicionado.
+     */
     void push(T element) {
         std::lock_guard<std::mutex> lg(m);
-        stk.push(element);
+        stk.push(std::make_shared<T>(element));
     }
 
-    void pop() {
+    /**
+     * @brief Remove e retorna o ponteiro compartilhado para o elemento no topo da pilha.
+     * @return Ponteiro compartilhado para o elemento no topo da pilha.
+     * @throws std::runtime_error se a pilha estiver vazia.
+     */
+    std::shared_ptr<T> pop() {
         std::lock_guard<std::mutex> lg(m);
+        if (stk.empty()) {
+            throw std::runtime_error("Stack is empty");
+        }
+        std::shared_ptr<T> res(stk.top());
+        stk.pop();
+        return res;
+    }
+
+    /**
+     * @brief Remove o elemento no topo da pilha e atribui seu valor a uma referência.
+     * @param value Referência para armazenar o valor do elemento removido.
+     * @throws std::runtime_error se a pilha estiver vazia.
+     */
+    void pop(T&value) {
+        std::lock_guard<std::mutex> lg(m);
+        if (stk.empty()) {
+            throw std::runtime_error("Stack is empty");
+        }
+        value = *(stk.top().get());
         stk.pop();
     }
 
+    /**
+     * @brief Retorna uma referência para o elemento no topo da pilha.
+     * @return Referência para o elemento no topo da pilha.
+     * @throws std::runtime_error se a pilha estiver vazia.
+     */
     T& top() {
         std::lock_guard<std::mutex> lg(m);
-        return stk.top();
+        if (stk.empty()) {
+            throw std::runtime_error("Stack is empty");
+        }
+        return *(stk.top());
     }
 
+    /**
+     * @brief Verifica se a pilha está vazia.
+     * @return True se a pilha estiver vazia, false caso contrário.
+     */
     bool empty() {
-
+        std::lock_guard<std::mutex> lg(m);
+        return stk.empty();
     }
 
+    /**
+     * @brief Retorna o tamanho atual da pilha.
+     * @return Tamanho atual da pilha.
+     */
     size_t size() {
-
+        std::lock_guard<std::mutex> lg(m);
+        return stk.size();
     }
 };
+
 
 #endif //STACK_THREAD_SAFE_H
